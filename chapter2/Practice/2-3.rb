4e60023b10b7064f5137c90d22f9f8808878e103
ms919 <sakuraimiki919@gmail.com>
2021-06-15 22:35:33 +0900

    finish Chapter2 Practice

---
+++ chapter2/Practice/2-3.rb
@@ -0,0 +1,5 @@
+#coffee = 300
+coffee = 400
+espresso = 100
+
+puts "コーヒー：#{coffee}円\n合計：#{coffee + espresso * 2}円"
---
+++ chapter2/Practice/2-6.rb
@@ -0,0 +1,4 @@
+total = 300 * 2
+p "コーヒ2杯：#{total}円"
+tax = total * 0.08
+puts tax
