9f9cd4430aa9d1ef867afb7c2f488d53c004af8b
ms919 <sakuraimiki919@gmail.com>
2021-06-17 22:47:57 +0900

    finish 8-1 (what is class?)

---
+++ chapter8/8-1/class1.rb
@@ -0,0 +1,13 @@
+p 1.class
+p 1.class.superclass
+p 1.class.superclass.superclass
+p 1.class.superclass.superclass.superclass
+p 1.class.superclass.superclass.superclass.superclass
+
+p "" == String.new
+p [] == Array.new
+p [] == Array.new()
+p [] == Array.new(0, "aaa")
+p Array.new(0, "aaa")
+p Array.new(1, "aaa")
+p Array.new(2, "aaa")
