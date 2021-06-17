cbfc175455f50c275242619bd818bc2339a839a1
ms919 <sakuraimiki919@gmail.com>
2021-06-17 16:16:00 +0900

    finish 6-1 (hash/symbol)

---
+++ chapter6/6-1/hash.rb
@@ -0,0 +1,7 @@
+p ( { :coffee => 300, :caffe_latte => 400 } )
+#p { :coffee => 300, :caffe_latte => 400 }
+menu_1 = { :coffee => 300, :caffe_latte => 400 }
+p menu_1
+
+menu_2 = { coffee: 300, caffe_latte: 400 }
+p menu_2[:coffee]
