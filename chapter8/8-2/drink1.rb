2e13cca1055b78d6fde8955398c787c7b87d5b49
ms919 <sakuraimiki919@gmail.com>
2021-06-17 23:04:44 +0900

    finish 8-2 (create class)

---
+++ chapter8/8-2/drink1.rb
@@ -0,0 +1,9 @@
+class Drink
+  
+end
+
+p Drink.class
+p Drink.class.superclass
+p Drink.class.superclass.superclass
+p Drink.class.superclass.superclass.superclass
+p Drink.class.superclass.superclass.superclass.superclass
